f9fdc843f8b4d3a159cd5fd2e901e13f7c315855
Drake <evansdr@oregonstate.edu>
2020-02-12 16:08:13 -0800

    cleaned up composer files as they were not needed.

--- js/3dmodel.js
+++ js/3dmodel.js
@@ -2,29 +2,38 @@ var camera, scene, renderer;
 var geometry, material, mesh;
 var controls;
 
+var frustumSize = 600;
+var canvasDims = document.getElementById("model_canvas").getBoundingClientRect();;
+var width = canvasDims.width;
+var height = canvasDims.height;
+var aspect = width/height;
+
 init();
 animate();
 
 function init() {
 
-	canvasDims = document.getElementById("model_canvas").getBoundingClientRect();
-
-	camera = new THREE.OrthographicCamera( canvasDims.width / - 2, canvasDims.width / 2, canvasDims.height / 2, canvasDims.height / - 2, 1, 1000 );
-	camera.position.z = 1;
-
 	scene = new THREE.Scene();
 	scene.background = new THREE.Color(0xffffff);
 
-	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+	camera = new THREE.OrthographicCamera( width / - 2, width / 2, height / 2, height / - 2, 1, 1000 );
+
+	camera.position.z = 5;
+
+	scene.add(camera);
+
+	geometry = new THREE.BoxGeometry(  );
 	material = new THREE.MeshNormalMaterial();
 
 	mesh = new THREE.Mesh( geometry, material );
 	scene.add( mesh );
 
 	renderer = new THREE.WebGLRenderer( { antialias: true, canvas: model_canvas } );
+	renderer.setSize(width, height);
 
 	controls = new THREE.OrbitControls(camera, document.getElementById("model_canvas"));
 
+	window.addEventListener( 'resize', onWindowResize, false );
 }
 
 function animate() {
@@ -37,4 +46,18 @@ function animate() {
 
 	renderer.render( scene, camera );
 
+}
+
+function onWindowResize(){
+
+	canvasDims = document.getElementById("model_canvas").getBoundingClientRect();;
+	width = canvasDims.width;
+	height = canvasDims.height;
+	aspect = width/height; 
+
+	camera.left = - width / 2;
+	camera.right = width / 2;
+	camera.top = height / 2;
+	camera.bottom = - height / 2;
+	camera.updateProjectionMatrix();
 }
