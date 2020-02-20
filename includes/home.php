515f547bf2397071ca35f9530df5d2b13cf65add
Peng Zhang <38088760+933039258@users.noreply.github.com>
2020-02-19 20:03:04 -0800

    homepage

--- includes/home.php
+++ includes/home.php
@@ -1,87 +1,60 @@
-<?php include("includes/config.php");?>
-<!DOCTYPE html>
-<html>
-<head>
-  <?php include("includes/head-contents.php");?>
 
-</head>
-
-<body>
 <div class="text-center">
     <h1>boxeur</h1>
-    <p>intro to this website</p>
-    <p>bla bla bla bla</p>
-
-</div>
-
-<div class="container">
-
-    <div class="row">
-        <div class="boxExample">
-            <div class="card" style="width: 18rem;">
-              <img class="card-img-top" src="..." alt="Card image cap">
-              <div class="card-body">
-                <h5 class="card-title"> Box Example </h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="editor.php" class="btn btn-primary">Clean project</a>
-              </div>
-            </div>
-        </div>
-        <div class="boxExample">
-            <div class="card" style="width: 18rem;">
-              <img class="card-img-top" src="..." alt="Card image cap">
-              <div class="card-body">
-                <h5 class="card-title">Box Example</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-        </div>
-        <div class="boxExample">
-            <div class="card" style="width: 18rem;">
-              <img class="card-img-top" src="..." alt="Card image cap">
-              <div class="card-body">
-                <h5 class="card-title">Box Example</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-        </div>
-
-        <div class="boxExample">
-            <div class="card" style="width: 18rem;">
-              <img class="card-img-top" src="..." alt="Card image cap">
-              <div class="card-body">
-                <h5 class="card-title">Box Example</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-        </div>
-        <div class="boxExample">
-            <div class="card" style="width: 18rem;">
-              <img class="card-img-top" src="..." alt="Card image cap">
-              <div class="card-body">
-                <h5 class="card-title">Box Example</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-        </div>
-        <div class="boxExample">
-            <div class="card" style="width: 18rem;">
-              <img class="card-img-top" src="..." alt="Card image cap">
-              <div class="card-body">
-                <h5 class="card-title">Box Example</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-        </div>
+    <p>this is some info about what the site is and how to use it</p>
+    <p>Log in or create an account to save your projects!</p>
+    <a class="btn btn-primary" href="login.php" role="button">Log In</a>
+    <!--<a class="btn btn-primary" href="" role="button">Create Account</a>-->
+    <div class="login">
+        <p>Log in with:</p>
+        <a class="google" href="login.php" role="button"></a>
+        <!--<a class="facebook" href="login.php" role="button"></a>-->
+        <!--<a class="twitter" href="login.php" role="button"> </a>-->
     </div>
 </div>
 
-<?php include("includes/footer.php");?>
 
-</body>
-</html>
+
+<div id="slider">
+
+    <div class="slider-inner">
+        <div id="slider-content">
+            <div class="meta">Boxeur</div>
+            <h2 id="slide-title">Boxeur<br>Boxeur</h2>
+            <span data-slide-title="0">Boxeur <br>Boxeur</span>
+            <span data-slide-title="1">Boxeur <br>Boxeur</span>
+            <span data-slide-title="2">Boxeur <br>Boxeur</span>
+            <span data-slide-title="3">Boxeur <br>Boxeur</span>
+            <div class="meta">Intro</div>
+            <div id="slide-status">Intro</div>
+            <span data-slide-status="0">Intro</span>
+            <span data-slide-status="1">Intro</span>
+            <span data-slide-status="2">Intro</span>
+            <span data-slide-status="3">Intro</span>
+        </div>
+    </div>
+
+    <img src="img/a5.png" />
+    <img src="img/a6.png" />
+    <img src="img/a5.png" />
+    <img src="img/a6.png" />
+
+    <div id="pagination">
+        <button class="active" data-slide="0"></button>
+        <button data-slide="1"></button>
+        <button data-slide="2"></button>
+        <button data-slide="3"></button>
+    </div>
+
+</div>
+
+
+<script type="text/javascript" src='js/three.min.js'></script>
+<script type="text/javascript" src='js/imagesloaded.pkgd.js'></script>
+<script type="text/javascript" src='js/TweenMax.min.js'></script>
+<script type="text/javascript" src="js/index.js"></script>
+
+
+
+
+
