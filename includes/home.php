d73890e9b1e6b8cd405830cb5ff9714a5f6b9ae8
Evan Hopper-Moore <hopperme@oregonstate.edu>
2020-02-20 21:41:44 -0800

    Redesign homepage

--- includes/home.php
+++ includes/home.php
@@ -1,60 +1,17 @@
-
-<div class="text-center">
-    <h1>boxeur</h1>
-    <p>this is some info about what the site is and how to use it</p>
-    <p>Log in or create an account to save your projects!</p>
-    <a class="btn btn-primary" href="login.php" role="button">Log In</a>
-    <!--<a class="btn btn-primary" href="" role="button">Create Account</a>-->
-    <div class="login">
-        <p>Log in with:</p>
-        <a class="google" href="login.php" role="button"></a>
-        <!--<a class="facebook" href="login.php" role="button"></a>-->
-        <!--<a class="twitter" href="login.php" role="button"> </a>-->
-    </div>
-</div>
-
-
-
 <div id="slider">
-
     <div class="slider-inner">
         <div id="slider-content">
-            <div class="meta">Boxeur</div>
-            <h2 id="slide-title">Boxeur<br>Boxeur</h2>
-            <span data-slide-title="0">Boxeur <br>Boxeur</span>
-            <span data-slide-title="1">Boxeur <br>Boxeur</span>
-            <span data-slide-title="2">Boxeur <br>Boxeur</span>
-            <span data-slide-title="3">Boxeur <br>Boxeur</span>
-            <div class="meta">Intro</div>
-            <div id="slide-status">Intro</div>
-            <span data-slide-status="0">Intro</span>
-            <span data-slide-status="1">Intro</span>
-            <span data-slide-status="2">Intro</span>
-            <span data-slide-status="3">Intro</span>
+            <div class="meta"></div>
+            <h2 id="slide-title">Boxeur</h2>
+            <h4>A 3D Case Designer</h4>
+            <div class="meta">How to use it</div>
+            <p style="color:white;">
+                Head over to the editor tab to make a case.
+                To save your projects online you can log in with a google account.
+                To export the project, go to the Project tab in the editor and press export.
+            </p>
         </div>
-    </div>
-
-    <img src="img/a5.png" />
-    <img src="img/a6.png" />
-    <img src="img/a5.png" />
-    <img src="img/a6.png" />
-
-    <div id="pagination">
-        <button class="active" data-slide="0"></button>
-        <button data-slide="1"></button>
-        <button data-slide="2"></button>
-        <button data-slide="3"></button>
+        <img src="img/a5.png" />
     </div>
 
 </div>
-
-
-<script type="text/javascript" src='js/three.min.js'></script>
-<script type="text/javascript" src='js/imagesloaded.pkgd.js'></script>
-<script type="text/javascript" src='js/TweenMax.min.js'></script>
-<script type="text/javascript" src="js/index.js"></script>
-
-
-
-
-
