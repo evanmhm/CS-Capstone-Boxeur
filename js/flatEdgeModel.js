873fcd55864a690730273342252dac2644fca238
Drake <evansdr@oregonstate.edu>
2020-05-15 19:09:07 -0700

    Code for circle and square hole mostly implemented. Circle hole doesn't cut a hole correctly for some reason.

--- js/flatEdgeModel.js
+++ js/flatEdgeModel.js
@@ -4,10 +4,10 @@ var flatBoxMesh = [];
 function flatEdgeModel(){
     geometryList[0] = flatBoxGeometry;
     for(var i=0; i<6; i++){
-		setUpBasicGeometry(i, 0);
-		setUpBoxProperties(i, 0);
+		  setUpBasicGeometry(i, 0);
+	  	setUpBoxProperties(i, 0);
 
-		flatBoxMesh[i] = new THREE.Mesh( flatBoxGeometry[i], material );
+		  flatBoxMesh[i] = new THREE.Mesh( flatBoxGeometry[i], material );
     }
     //geometryList[0] = flatBoxGeometry;
     meshList[0] = flatBoxMesh;
